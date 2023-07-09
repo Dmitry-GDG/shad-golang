72d40ba720cfa251c90d8710011e936138e21f90
dmitry gatilov <dmigatilov@gmail.com>
2023-07-09 22:30:50 +0300

    hotelbusiness

--- shad-go/hotelbusiness/hotels.go
+++ shad-go/hotelbusiness/hotels.go
@@ -13,5 +13,36 @@ type Load struct {
 }
 
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	var dateMin, dateMax int
+	var outpLoads []Load
+	loads := make(map[int]int)
+	if len(guests) > 0 {
+		dateMin = guests[0].CheckInDate
+		dateMax = guests[0].CheckOutDate
+	} else {
+		return []Load{}
+	}
+	for i := 0; i < len(guests); i++ {
+		if guests[i].CheckInDate < dateMin {
+			dateMin = guests[i].CheckInDate
+		}
+		if guests[i].CheckOutDate > dateMax {
+			dateMax = guests[i].CheckOutDate
+		}
+		for date := guests[i].CheckInDate; date < guests[i].CheckOutDate; date++ {
+			loads[date]++
+		}
+	}
+	loads[dateMax] = 0
+	outpLoads = append(outpLoads, Load{dateMin, loads[dateMin]})
+	if len(loads) > 1 {
+		for date := dateMin + 1; date <= dateMax; date++ {
+			if loads[date] == loads[date - 1] {
+				continue
+			} else {
+				outpLoads = append(outpLoads, Load{date, loads[date]})
+			}
+		}
+	}
+	return outpLoads
 }
